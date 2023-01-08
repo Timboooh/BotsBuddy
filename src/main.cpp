787e97b424c0dc1a7fa9d6f9bcbee97341e9aac6
TiSO2 <robinvanantwerpen@gmail.com>
2023-01-08 15:43:09 +0100

    Accelerometer + lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
+#include "accel.hpp"
+
+long lastUpdate = 0;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  ACCEL::setup();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    long now = millis();
+    if (now - lastUpdate > 2000)
+    {
+        lastUpdate = now;
+        ACCEL::update();
+    }
 }
