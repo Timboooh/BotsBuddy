7334efb3cbff5be5b05d4b6bd0beb4630ac4f4be
Hubert <0973362@hr.nl>
2023-01-18 12:33:18 +0100

    Oled 0.1

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,23 @@
 #include <Arduino.h>
 #include "accel.hpp"
+#include "oled.hpp"
 
 long lastUpdate = 0;
+int counter = 21;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  ACCEL::setup();
+  //ACCEL::setup();
+  OLED::setup();
 }
 
 void loop() {
     long now = millis();
-    if (now - lastUpdate > 2000)
+    if (now - lastUpdate > 1000)
     {
         lastUpdate = now;
-        ACCEL::update();
+        //ACCEL::update();
+        OLED::startCountdown();
     }
 }
